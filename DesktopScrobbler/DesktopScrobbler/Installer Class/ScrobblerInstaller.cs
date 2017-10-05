55e26331b3b80ac168893a6edf43b2798c9d7ca3
VorTechS <vortechs@blueyonder.co.uk>
2017-10-05 09:48:48 +0100

    Installer now automatically starts the Scrobbler once installation is complete.

---
+++ DesktopScrobbler/DesktopScrobbler/Installer Class/ScrobblerInstaller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesktopScrobbler.Installer_Class
+{
+    [RunInstaller(true)]
+    public partial class ScrobblerInstaller : System.Configuration.Install.Installer
+    {
+        public ScrobblerInstaller()
+        {
+            InitializeComponent();
+        }
+
+        public override void Commit(IDictionary savedState)
+        {
+            System.Diagnostics.Process.Start(System.IO.Path.GetDirectoryName(this.Context.Parameters["AssemblyPath"]) + @"\DesktopScrobbler.exe");
+        }
+    }
+}

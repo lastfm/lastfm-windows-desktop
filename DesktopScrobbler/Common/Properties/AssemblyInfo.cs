a3f66e66ac5d6f628e25d1b041c8c4912eb20007
VorTechS <vortechs@blueyonder.co.uk>
2017-10-13 17:07:47 +0100

    Updated build task to update FileVersion property for MSI compatibility

--- DesktopScrobbler/Common/Properties/AssemblyInfo.cs
+++ DesktopScrobbler/Common/Properties/AssemblyInfo.cs
@@ -32,4 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.13.34")]
+[assembly: AssemblyVersion("3.1.13.35")]
+[assembly: AssemblyFileVersion("3.1.13.1")]
--- DesktopScrobbler/LastFMApi/Properties/AssemblyInfo.cs
+++ DesktopScrobbler/LastFMApi/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.13.34")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.1.13.35")]
+[assembly: AssemblyFileVersion("3.1.13.1")]
